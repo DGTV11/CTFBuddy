dae6ca363a15e45f73b3afb2a2f99551d1c09d08
DGTV11 <danielweedk@gmail.com>
2024-05-29 15:34:37 +0800

    Cleaned up and changed variable names in ctfbuddy/main.py

--- ctfbuddy/main.py
+++ ctfbuddy/main.py
@@ -1,82 +1,59 @@
 import gradio as gr
 
-import modules.generate_method_suggestions as gen_mtsu
+from modules.autosolver_categories.crypto_autosolver import crypto_autosolver
 
 if __name__ == "__main__":
     with gr.Blocks(
         theme=gr.themes.Default(primary_hue="red"),
         analytics_enabled=False,
         title="CTFBuddy",
-    ) as studyCopilot:
+    ) as ctfbuddy:
         gr.Markdown(
             """
 		# CTFBuddy
         """
         )
 
-        with gr.Tab("Method Suggestor"):
-            chall_name = gr.Textbox(label="Challenge name:")
-            flag_format = gr.Textbox(label="Flag format:")
-            chall_desc = gr.Textbox(label="Challenge description:")
-            chall_filepaths = gr.File(
+        with gr.Tab("Crypto Autosolver"):
+            ca_chall_name = gr.Textbox(label="Challenge name:")
+            ca_flag_format = gr.Textbox(label="Flag format:")
+            ca_chall_desc = gr.Textbox(label="Challenge description:")
+            ca_chall_filepaths = gr.File(
                 label="Challenge files (text):",
                 file_count="multiple",
                 file_types=["text"],
             )
 
             with gr.Row():
-                clear_mtsu_in_btn = gr.ClearButton(
+                clear_crypto_autosolver_in_btn = gr.ClearButton(
                     value="Clear inputs",
-                    components=[chall_name, flag_format, chall_desc, chall_filepaths],
+                    components=[ca_chall_name, ca_flag_format, ca_chall_desc, ca_chall_filepaths],
                 )
-                generate_mtsu_btn = gr.Button(value="Generate method suggestions")
+                ca_run_btn = gr.Button(value="Solve!")
 
-            method_suggestor_model = gr.Dropdown(
-                ["llama3", "openchat", "mistral", "phi3"], label="Model:", value="phi3"
+            ca_autosolver_model = gr.Dropdown(
+                ["llama3", "openchat", "mistral", "phi3"], label="Model:", value="mistral"
             )
-            """
-            with gr.Accordion("Extracted clues", open=True):
-                extracted_clues = gr.Markdown()
-            with gr.Accordion("Technique explanation", open=True):
-                technique_explanation = gr.Markdown()
-            with gr.Accordion("Suggested method", open=True):
-                suggested_method = gr.Markdown()
-            with gr.Accordion("Suggested solve script", open=True):
-                suggested_solve_script = gr.Markdown()
-            """
+            
             with gr.Column():
-                conv_token_frac = gr.Textbox(
+                ca_used_token_info = gr.Textbox(
                     label="[Used tokens]/[Context window] ([Used token percentage])"
                 )
-                chatbot = gr.Chatbot(label="Output")
+                ca_chatbox = gr.Chatbot(label="Output")
 
-            """
-            clear_mtsu_out_btn = gr.ClearButton(
-                value="Clear outputs",
-                components=[extracted_clues, technique_explanation, suggested_method, suggested_solve_script],
-            )
-            """
-
-        """
-        generate_mtsu_btn.click(
-            fn=gen_mtsu.generate_mtsu,
-            inputs=[method_suggestor_model, chall_name, flag_format, chall_desc, chall_filepaths],
-            outputs=[extracted_clues, technique_explanation, suggested_method, suggested_solve_script],
-        )
-        """
-        generate_mtsu_btn.click(
-            fn=gen_mtsu.generate_mtsu,
+        ca_run_btn.click(
+            fn=crypto_autosolver,
             inputs=[
-                method_suggestor_model,
-                chall_name,
-                flag_format,
-                chall_desc,
-                chall_filepaths,
-                chatbot,
+                ca_autosolver_model,
+                ca_chall_name,
+                ca_flag_format,
+                ca_chall_desc,
+                ca_chall_filepaths,
+                ca_chatbox,
             ],
-            outputs=[chatbot, conv_token_frac],
+            outputs=[ca_chatbox, ca_used_token_info],
         )
 
     print("Launching...")
-    studyCopilot.queue()
-    studyCopilot.launch()
+    ctfbuddy.queue()
+    ctfbuddy.launch()
